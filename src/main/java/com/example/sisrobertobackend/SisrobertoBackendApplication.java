afe699f8f0438096622adbd93f28ac80adfbbb14
Alex Moraes <projetogit2020@gmail.com>
2020-06-03 15:52:03 -0300

    versão Inicial Backend

---
+++ src/main/java/com/example/sisrobertobackend/SisrobertoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.sisrobertobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisrobertoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisrobertoBackendApplication.class, args);
+	}
+
+}
